d24ef05a6b61e839b619738b37ed1bf5f767885d
Đặng Huy Diệu <danghuydieu1243@gmail.com>
2025-01-06 15:29:31 +0700

    add profile Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,69 +6,53 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/anhdaidien.jpg')} // Đường dẫn ảnh đại diện của bạn
+                    style={styles.avatar}
+                />
+            }
+        >
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type='title'>Hi, I'm Dang Huy Dieu! </ThemedText>
+            </ThemedView>
+            <ThemedView style={styles.infoContainer}>
+                <ThemedText type='subtitle'>About Me:</ThemedText>
+                <ThemedText>
+                    I'm a student at HCMC University of Technology and Education, majoring in Software Technology.
+                </ThemedText>
+                <ThemedText>
+                    My student ID is<ThemedText type='defaultSemiBold'>21110818</ThemedText>.
+                </ThemedText>
+            </ThemedView>
+            
+        </ParallaxScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    infoContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    avatar: {
+        height: 178,
+        width: 178,
+        borderRadius: 89, // Để tạo ảnh hình tròn
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
 });
